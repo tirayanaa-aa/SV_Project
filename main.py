e73fe6e4a2e4af09d552fb0c117d84d73e0b45f5
tirayanaa-aa <tirayanaa@gmail.com>
2026-01-02 21:16:06 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import app  # Main app with navigation
+
+# Streamlit main run
+if __name__ == "__main__":
+    st.set_page_config(page_title="TikTok Shop Impulse Buying Visualization", layout="wide")
+    app  # Runs app.py
