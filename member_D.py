5acb0c84d7e007a05e6145a6fc9b8b3113b6772c
tirayanaa-aa <tirayanaa@gmail.com>
2026-01-03 12:42:40 +0800

    Update member_D.py

--- member_D.py
+++ member_D.py
@@ -8,7 +8,7 @@ def app():
        # --------------------------------------------------
     # Load dataset
     # --------------------------------------------------
-    df = pd.read_excel("cleaned_dataset_athirah.xlsx")
+    df = pd.read_excel("tiktok_impulse_buying_cleaned.csv")
 
 
     # =========================
